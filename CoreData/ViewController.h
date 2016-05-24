0322accf17ea113dc89552f30525d16aed63f942
咪咕 <dingleilei@cmvideo.cn>
2016-05-24 10:21:52 +0800

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by 咪咕 on 16/5/24.
+//  Copyright © 2016年 咪咕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by 咪咕 on 16/5/24.
+//  Copyright © 2016年 咪咕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
